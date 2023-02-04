b5467ba7d0a5c228b1d6d95237eb1a1fac4db680
adamar <aysun114@hotmail.com>
2023-02-04 10:40:33 -0600

    TestBase class added

---
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.base;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    }
+
+    @AfterMethod
+    public void teardownMethod(){
+        driver.quit();
+    }
+}
