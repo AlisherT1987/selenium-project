1c26d48120b19974ed0413c77431f1cf5dcd53d8
adamar <aysun114@hotmail.com>
2023-02-04 10:27:22 -0600

    HardVsSoftAssertion class added

--- src/test/java/com/cydeo/tests/review/week3/HardVsSoftAssertion.java
+++ src/test/java/com/cydeo/tests/review/week3/HardVsSoftAssertion.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -89,6 +90,9 @@ public class HardVsSoftAssertion {
 
     }
 
-
+    @AfterMethod
+    public void teardownMethod(){
+        driver.quit();
+    }
 
 }
