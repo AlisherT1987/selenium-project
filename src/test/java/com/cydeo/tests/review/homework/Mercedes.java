70c26fe9c0291db1aab9cbdcb47e6502856fdf68
AdamB25 <barsialtunit@gmail.com>
2023-02-11 23:49:27 +0300

    week 4 JavaScriptExecutor example

---
+++ src/test/java/com/cydeo/tests/review/homework/Mercedes.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.review.homework;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class Mercedes {
+    //homework
+
+    //try to click Buy online
+
+
+    @Test
+    public void test1() {
+        Driver.getDriver().get("https://www.mercedes-benz.co.uk/");
+    }
+}
